9a40eb7dcee17bdac641aa995d8207a991bc258b
magomedgadjiev <magomedalievic@gmail.com>
2016-11-06 18:05:09 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -2,10 +2,11 @@
 #include <catch.hpp>
 #include <iostream>
 
-// SCENARIO("ctor1", "[ctor1]"){
+SCENARIO("ctor1", "[ctor1]"){
 // 	  shared_ptr<int> sp;
 // 	  REQUIRE(!sp.get());
-// }
+    REQUIRE(true);
+}
 
 // SCENARIO("ctor2", "[ctor2]"){
 // 	  shared_ptr<int> sp(new int(7));
